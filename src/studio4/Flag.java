f3f95c39e5188810ea77153e708e2f61f1fb6fa7
24kev <24kev@KevinOu.dhcp.wustl.edu>
2024-10-03 12:35:55 -0500

    done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,16 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setCanvasSize(570,300);
+		StdDraw.setXscale(0,1);
+		StdDraw.setYscale(0,1);
+		
+		StdDraw.setPenColor(Color.red);
+		StdDraw.filledRectangle(570/2.0, 369/16.0, 570/2.0, 369/32.0);
+		
+		
+		
+		
+		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 import javax.swing.JFileChooser;
 import javax.swing.SwingUtilities;
-
+import java.awt.Color;
 import edu.princeton.cs.introcs.StdDraw;
 
 /**
@@ -19,6 +19,50 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean filled = in.nextBoolean();
+
+		Color penColor = new Color(red,green,blue);
+		StdDraw.setPenColor(penColor);
+
+		if(shape.equals("rectangle")) {
+			double xPos = in.nextDouble();
+			double yPos = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halflength = in.nextDouble();
+			if(filled == true) {
+				StdDraw.filledRectangle(xPos, yPos, halfWidth, halflength);
+			}else {
+				StdDraw.rectangle(xPos, yPos, halfWidth, halflength);
+			}
+		}else if(shape.equals("ellipse")) {
+			double xPos = in.nextDouble();
+			double yPos = in.nextDouble();
+			double majorAxis = in.nextDouble();
+			double minorAxis = in.nextDouble();
+			if (filled == true) {
+				StdDraw.filledEllipse(xPos, yPos, majorAxis, minorAxis);
+			}else {
+				StdDraw.ellipse(xPos, yPos, majorAxis, minorAxis);
+			}
+		}else {
+			double xPos1 = in.nextDouble();
+			double yPos1 = in.nextDouble();
+			double xPos2 = in.nextDouble();
+			double yPos2 = in.nextDouble();
+			double xPos3 = in.nextDouble();
+			double yPos3 = in.nextDouble();
+			double[] x = {xPos1, xPos2, xPos3};
+			double[] y = {yPos1, yPos2, yPos3};
+			if (filled == true) {
+				StdDraw.filledPolygon(x, y);
+			}else {
+				StdDraw.polygon(x, y);
+			}
+		}
 	}
 }
